122d8b72b0f70d201bacdf99bdfdf0be9d4cd581
Travis Spangle <tspangle@Travis-Spangles-MacBook.local>
2011-04-11 11:25:14 -0700

    Cleaned and commented NSTask example code.

--- NSTaskExample/NSTaskExampleAppDelegate.m
+++ NSTaskExample/NSTaskExampleAppDelegate.m
@@ -21,12 +21,15 @@
 {
     NSLog(@"%s\n\tPath Value:%@",__PRETTY_FUNCTION__,[pathToList stringValue]);
     
+    //setting the command to be run from the /bin/ls directory
     NSTask *lsCommand = [[NSTask alloc] init];
     [lsCommand setLaunchPath:@"/bin/ls"];
     
+    //command arguments ls -la [path]
     NSArray *lsArgs = [NSArray arrayWithObjects: @"-l", @"-a", [pathToList stringValue], nil];
     [lsCommand setArguments:lsArgs];
     
+    //creating pipe object and file handle to collect command outoput 
     NSPipe *pipe;
     pipe = [NSPipe pipe];
     [lsCommand setStandardOutput: pipe];
@@ -34,16 +37,15 @@
     NSFileHandle *file;
     file = [pipe fileHandleForReading];
     
-
+    //Run command
     [lsCommand launch];
     [lsCommand waitUntilExit];
-     
-    NSData *data;
-    data = [file readDataToEndOfFile];
-    
+
+    //collect output
     NSString *output;
-    output = [[[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding] autorelease];
+    output = [[[NSString alloc] initWithData: [file readDataToEndOfFile] encoding: NSUTF8StringEncoding] autorelease];
     
+    //display on text field
     [displayDirectory setStringValue:output];
     [lsCommand release];
 }
