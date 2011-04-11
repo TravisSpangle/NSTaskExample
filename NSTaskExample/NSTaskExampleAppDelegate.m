10cd9a53fbab95165328587e2e5f5e8fa4a43a77
Travis Spangle <tspangle@Travis-Spangles-MacBook.local>
2011-04-11 10:37:00 -0700

    Implemented NSTask functionality and tied to UI elements.

--- NSTaskExample/NSTaskExampleAppDelegate.m
+++ NSTaskExample/NSTaskExampleAppDelegate.m
@@ -19,10 +19,33 @@
 
 - (IBAction)getPath:(id)sender
 {
-    NSLog(@"%s\n\tPathValue:%@",__PRETTY_FUNCTION__,[pathToList stringValue]);
+    NSLog(@"%s\n\tPath Value:%@",__PRETTY_FUNCTION__,[pathToList stringValue]);
     
-    [displayDirectory setStringValue:@"See This"];
+    NSTask *lsCommand = [[NSTask alloc] init];
+    [lsCommand setLaunchPath:@"/bin/ls"];
+    
+    NSArray *lsArgs = [NSArray arrayWithObjects: @"-l", @"-a", [pathToList stringValue], nil];
+    [lsCommand setArguments:lsArgs];
+    
+    NSPipe *pipe;
+    pipe = [NSPipe pipe];
+    [lsCommand setStandardOutput: pipe];
+    
+    NSFileHandle *file;
+    file = [pipe fileHandleForReading];
+    
 
+    [lsCommand launch];
+    [lsCommand waitUntilExit];
+     
+    NSData *data;
+    data = [file readDataToEndOfFile];
+    
+    NSString *output;
+    output = [[[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding] autorelease];
+    
+    [displayDirectory setStringValue:output];
+    [lsCommand release];
 }
 
 @end
